af2d8f7edbc376829a34cb1cf9c3a849dcf76210
zzettita <jpg3020@gmail.com>
2022-12-04 23:55:06 +0100

    Initial commit

---
+++ src/main/java/net/zzettita/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.zzettita.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
