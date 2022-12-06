34638e32bb62aa6e1f798b0ced79730921178b1b
zzettita <jpg3020@gmail.com>
2022-12-06 16:36:49 +0100

    added tanzanite block, ore and deepslate ore

--- src/main/java/net/zzettita/tutorialmod/TutorialMod.java
+++ src/main/java/net/zzettita/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.zzettita.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.zzettita.tutorialmod.block.ModBlocks;
 import net.zzettita.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,5 +13,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/zzettita/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,42 @@
+package net.zzettita.tutorialmod.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.util.registry.Registry;
+import net.zzettita.tutorialmod.TutorialMod;
+import net.zzettita.tutorialmod.item.ModItemGroup;
+
+public class ModBlocks {
+
+    public static final Block TANZANITE_BLOCK = registerBlock("tanzanite_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()), ModItemGroup.TANZANITE);
+    public static final Block TANZANITE_ORE = registerBlock("tanzanite_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(),
+                    UniformIntProvider.create(3,7)), ModItemGroup.TANZANITE);
+    public static final Block DEEPSLATE_TANZANITE_ORE = registerBlock("deepslate_tanzanite_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(),
+                    UniformIntProvider.create(3,7)), ModItemGroup.TANZANITE);
+
+    private static Block registerBlock(String name, Block block, ItemGroup tab) {
+        registerBlockItem(name, block, tab);
+        return Registry.register(Registry.BLOCK, new Identifier(TutorialMod.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block, ItemGroup tab) {
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(tab))); //item asociado a un bloque
+    }
+
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.debug("registering ModBlocks for " + TutorialMod.MOD_ID);
+    }
+
+}
